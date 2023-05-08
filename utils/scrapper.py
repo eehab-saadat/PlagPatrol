d75fcde115000d8036f790d49dac2409ff5fdc6c
akcena14 <116361254+akcena14@users.noreply.github.com>
2023-05-08 12:26:00 +0500

    Update scrapper.py
    added 4 api which will change when http error occurs

--- utils/scrapper.py
+++ utils/scrapper.py
@@ -2,8 +2,8 @@ from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
 # Enter your custom search engine ID and API key here
-SEARCH_ENGINE_ID = '05a0718899f664fda'
-API_KEY = 'AIzaSyAe34tm9-Og0El5ISwScopDSlYspE-XwO0'
+SEARCH_ENGINE_ID = '22f500feeb8a446df'
+API_KEY = ['AIzaSyAe34tm9-Og0El5ISwScopDSlYspE-XwO0','AIzaSyBOA0brUC0brGOUafT9-b9iRqNInGCAElw','AIzaSyD_HGYh0pQ3jk-VSVD-QSzR5Htf9at0p1E', 'AIzaSyBAfORIz0-AwPGogtQ9XX64qVx1xBJWj50']
 
 
 def check_plagiarism(phrases, word_count):
@@ -11,19 +11,19 @@ def check_plagiarism(phrases, word_count):
     Check the given list of phrases for plagiarism and return a tuple containing the
     percentage of plagiarized content and a dictionary with the original and plagiarism
     sources.
-
     Args:
         phrases: A list of phrases to check for plagiarism.
         word_count: Total number of words in the document to be checked
-
     Returns:
         A tuple containing a float and a dictionary with string-string key value pairs.
         The float represents the percentage of plagiarized content (as a value between 0
         and 1), and the dictionary has the original phrases as keys and the plagiarism
         sources as values.
     """
+    #initializing key number
+    key_number = 0
     # Build the service object for the Custom Search JSON API
-    service = build('customsearch', 'v1', developerKey=API_KEY)
+    service = build('customsearch', 'v1', developerKey=API_KEY[key_number])
 
     # Initialize an empty dictionary to store the search results
     Dict = {}
@@ -35,24 +35,42 @@ def check_plagiarism(phrases, word_count):
         #  adding qoutes to query for searching
         search = f'"{phrase}"'
         try:
-
-            if len(phrase.split(' ', 2)) > 2:
-                # getting first search result
-                response = service.cse().list(q=search, cx=SEARCH_ENGINE_ID, num=1).execute()
-                # checking if any result was found
-                if 'items' in response:
-                    first_url = response['items'][0]['link']
-                    # adding phrase plus url to dict
-                    Dict[phrase] = first_url
-                    # adding number of words in phrase to found variable
-                    found += len(phrase.split())
-                else:
-                    # adding null value if phrase not found
-                    Dict[phrase] = ""
+            # getting first search result
+            response = service.cse().list(q=search, cx=SEARCH_ENGINE_ID, num=1).execute()
+            # checking if any result was found
+            if 'items' in response:
+                first_url = response['items'][0]['link']
+                # adding phrase plus url to dict
+                Dict[phrase] = first_url
+                # adding number of words in phrase to found variable
+                found += len(phrase.split())
+            else:
+                # adding null value if phrase not found
+                Dict[phrase] = ""
         except HttpError as error:
             print(f'An error occurred: {error}')
-            Dict[phrase] = None
+            key_number+=1
+            # Build the service object for the Custom Search JSON API with new API key
+            service = build('customsearch', 'v1', developerKey=API_KEY[key_number])
+            continue
+
 
     plag_index = format(found / word_count, ".3f")
     result = (plag_index, Dict)
     return result
+
+
+# Test the function with a sample query
+
+
+filename = 'input.txt'
+with open(filename, 'r', encoding='utf-8') as f:
+    data = f.read()  # Read the entire file into a string variable
+wordcount = len(data.split(' ' or '.'))
+sentences = data.split('.')  # Split the string into sentences based on full stops
+sentences.pop()
+print(sentences)
+print(wordcount)
+result = (check_plagiarism(sentences, wordcount))  # call function and constantly update dictionary
+
+print(result)
